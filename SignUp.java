7c4c6e766a930bf996f4708cf82db6e13ece7c49
Merve Nur <114695563+iamnurrr@users.noreply.github.com>
2022-12-12 22:50:02 +0300

    Sign İn for  55

---
+++ SignUp.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class SignUp
+    {public static void main(String[] args) {
+
+    Scanner scn=new Scanner(System.in);
+        int password;
+        int userno ;
+
+        System.out.println("Please enter your userno :");
+        userno=scn.nextInt();
+        System.out.println("Please enter your password :");
+        password= scn.nextInt();
+
+        if(userno==55 && password==8877){
+            System.out.println("Login.. ");}
+            else {
+            System.out.println("Wrong userno or password!! ");
+        }
+
+
+
+
+
+
+
+
+
+        }
+
+
+     }
+
+
+
+
+
+
+
