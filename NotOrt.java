d81ed24ce064f79415b6b8990611bd4c06e92141
Merve Nur <114695563+iamnurrr@users.noreply.github.com>
2022-12-12 21:41:24 +0300

    Not ortalaması  hesaplayan program

---
+++ NotOrt.java
@@ -0,0 +1,57 @@
+
+import java.util.Scanner;
+public class NotOrt {
+    public static void main(String[] args) {
+    int numberOfLesson=0;
+        int avange=0;
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Enter your math grade: ");
+        int math = scn.nextInt();
+
+        System.out.println("Enter your Turkish grade: ");
+        int Turkish = scn.nextInt();
+
+        System.out.println("Enter your physic grade: ");
+        int phy = scn.nextInt();
+
+        System.out.println("Enter your biology grade: ");
+        int bio = scn.nextInt();
+
+        System.out.println("Enter your chemistry grade: ");
+        int che = scn.nextInt();
+
+        if (math!=0&&math!=100) {
+            avange+=math;
+            numberOfLesson++;}
+            if (phy!=0&&phy!=100) {
+               avange+=phy;
+                numberOfLesson++;}
+                if (Turkish!=0&&Turkish!=100) {
+                   avange+=Turkish;
+                numberOfLesson++;}
+                if ((bio!=0&&bio!=100)){
+                    avange+=bio;
+                    numberOfLesson++;}
+                if(che!=0&&che!=100) {
+                    avange += che;
+                    numberOfLesson++;
+                    System.out.println("Derslerin Ortalaması: " + (avange / numberOfLesson));}
+                else {
+                    System.out.println("Ortalamanız Hesaplanamaz!");
+
+
+
+               }
+
+
+               }
+
+
+                }
+
+
+
+
+
+
