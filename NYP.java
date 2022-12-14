11cb4001f90dd83fc0358b821c270549b38595fe
Merve Nur <114695563+iamnurrr@users.noreply.github.com>
2022-12-14 23:26:10 +0300

    İstenen çıktıyı ekrana yazdırma

---
+++ NYP.java
@@ -0,0 +1,21 @@
+public class NYP {
+    public static void main(String[] args) {
+
+        int dizi[]=new int[]{0,0,0,0,0,0,1,2,4,2,1};
+        for (int i=0;i<dizi.length;i++){
+            if(i==10){
+                System.out.println("100:");
+            }
+            else{
+                System.out.println(i*10+"-"+((i*10)+9)+":");
+            }
+            for (int j=0;j<dizi[i];j++){
+                System.out.println("*");
+                //System.out.println("\n");
+            }
+
+        }
+
+    }
+
+}
