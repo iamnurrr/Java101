6e7b9f351387a980083b7c1244c5238898d4c099
Merve Nur <114695563+iamnurrr@users.noreply.github.com>
2022-12-14 23:28:00 +0300

    Update NYP.java

--- NYP.java
+++ NYP.java
@@ -1,6 +1,7 @@
 public class NYP {
     public static void main(String[] args) {
-
+//İstenen çıktı resim olarak eklendi
+        
         int dizi[]=new int[]{0,0,0,0,0,0,1,2,4,2,1};
         for (int i=0;i<dizi.length;i++){
             if(i==10){
