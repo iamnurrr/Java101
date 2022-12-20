8670a23dcf8e9d8f7d542907becca25b324f39d3
Merve Nur <114695563+iamnurrr@users.noreply.github.com>
2022-12-20 19:42:20 +0300

    iki matrisi çarpan program

---
+++ MatrisCarpimi.java
@@ -0,0 +1,26 @@
+public class MatrisCarpimi {
+    public static void main(String[] args) {
+        int[][]matris1={{1,2},{3,4}};   //     1 2     10 20
+        int[][]matris2={{10,20},{30,40}};//    3 4     30 40
+        int i,j;
+        int carpim[][];
+        carpim=carp(matris1,matris2);
+        for(i=0;i<2;i++){
+            for (j=0;j<2;j++){
+                System.out.println(carpim[i][j]);
+            }
+        }
+    }
+    public static int [][]carp(int[][]m1,int[][]m2){
+        int i,j,k;
+        int [][] sonuc=new int[m1[0].length][m2.length];
+        for(i=0;i<2;i++){
+                for (k=0;k<2;k++){
+                    for (j=0;j< 2;j++){
+                        sonuc[i][k]+=m1[i][j]*m2[j][k];
+                    }
+            }
+        }
+return sonuc;
+    }
+}
