d45734787657e7e74ba50d8efd039864ab2130a5
Merve Nur <114695563+iamnurrr@users.noreply.github.com>
2023-07-05 20:02:25 +0300

    Çok Boyutlu Diziler ile A Harfi Yazdıran Program

---
+++ AharfiYazdir.java
@@ -0,0 +1,23 @@
+public class AharfiYazdir {
+    public static void main(String[] args) {
+        String list[][] = new String[6][4];
+        int i, j;
+        for (i = 0; i < list.length; i++) {
+            for (j = 0; j < list[i].length; j++) {
+                if (i == 0 || i == 2) {
+                    list[i][j] = " * ";
+                } else if (j == 0 || j == 3) {
+                    list[i][j] = " * ";
+                } else
+                    list[i][j] = "   ";
+            }
+        }
+        for (i = 0; i < list.length; i++) {
+            System.out.println();
+            for (j = 0; j < list[i].length; j++) {
+                System.out.print(list[i][j]);
+            }
+        }
+    }
+
+}
